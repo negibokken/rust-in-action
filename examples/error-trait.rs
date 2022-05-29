039c0efe66893966aa74a4adad5dabe344030cb8
Ryo Ogawa <negibokken@gmail.com>
2022-05-29 13:19:26 +0900

    Update error example with trait object

--- examples/error-trait.rs
+++ examples/error-trait.rs
@@ -1,11 +1,30 @@
+use std::error;
+use std::fmt;
+use std::io;
+use std::net;
 use std::fs::File;
-use std::error::Error;
 use std::net::Ipv6Addr;
-fn main () -> Result<(),Box<dyn Error>>{
 
-    let _f = File::open("invisible.txt")?;
+#[derive(Debug)]
+enum UpstreamError {
+    IO(io::Error),
+    Parsing(net::AddrParseError),
+}
 
-    let _localhost = "::1".parse::<Ipv6Addr>()?;
+impl fmt::Display for UpstreamError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl error::Error for UpstreamError {}
+
+
+fn main () -> Result<(),UpstreamError>{
+
+    let _f = File::open("invisible.txt").map_err(UpstreamError::IO)?;
+
+    let _localhost = "::1".parse::<Ipv6Addr>().map_err(UpstreamError::Parsing)?;
 
     Ok(())
 }
